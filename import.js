27d9ed89c02e332fe42e004d4f44f0bc73eb07c6
Enrique Casanova <ecasanova@webfactorystudio.co>
2022-11-21 17:58:29 -0500

    Refactor solution

--- import.js
+++ import.js
@@ -3,31 +3,32 @@ const csvtojson = require("csvtojson");
 let data = csvtojson()
   .fromFile("input.csv")
   .then(async (data) => {
-    let parsed = data.reduce((m, o) => {
-      let found = m.find(
-        (p) =>
-          p.name === o.name &&
-          p.lastname === o.lastname &&
-          p.position === o.position &&
-          p.date === o.date
-      );
-      if (found) {
-        found.technology += ", " + o.technology;
-      } else {
-        m.push(o);
-      }
-      return m;
-    }, []);
-    let returned = parsed.map((item) => {
-      return {
-        name: item.name,
-        lastname: item.lastname,
-        position: item.position,
-        fav_tech: item.technology,
-        age: calculateAge(item.date),
-      };
-    });
-    console.log(returned);
+    let parsed = data
+      .reduce((m, o) => {
+        let found = m.find(
+          (p) =>
+            p.name === o.name &&
+            p.lastname === o.lastname &&
+            p.position === o.position &&
+            p.date === o.date
+        );
+        if (found) {
+          found.technology += ", " + o.technology;
+        } else {
+          m.push(o);
+        }
+        return m;
+      }, [])
+      .map((item) => {
+        return {
+          name: item.name,
+          lastname: item.lastname,
+          position: item.position,
+          fav_tech: item.technology,
+          age: calculateAge(item.date),
+        };
+      });
+    console.log(parsed);
   });
 
 function calculateAge(dob_provided) {
